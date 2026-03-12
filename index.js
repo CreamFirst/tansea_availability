8a817600d6f4f53864947221f777fe46afcd652e
CreamFirst <toby@robinsondeux.co.uk>
2026-03-12 17:28:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+ res.send("Tansea availability service running");
+});
+
 // ---------------------------
 // CONFIG
 // ---------------------------
